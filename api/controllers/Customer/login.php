2bde0a5f3f383458ac2dd7e2df3079224c12ee9c
elhajuojy <souirimehdi311@gmail.com>
2023-01-31 19:54:33 +0100

    login logic done

--- api/controllers/Customer/login.php
+++ api/controllers/Customer/login.php
@@ -7,16 +7,21 @@ use core\Response;
 $db = App::resolve(Database::class);
 
 
+// parse_str(file_get_contents('php://input'),$_delete);
+// dd($_delete);
 
-$Customer_reference = $_POST['Customer_reference'];
+
+$Customer_reference = filterRequest($_POST['Customer_reference']);
+
 
 $Customer = $db->query("SELECT * FROM Customer WHERE Customer_reference = :Customer_reference", ['Customer_reference' => $Customer_reference])->statement->fetch();
 
 
 if($Customer){
+
     http_response_code(Response::OK);
+    setcookie($Customer_reference, time() + (86400 * 30), "/");
     echo json_encode($Customer);
-    
 }
 
 else{
