11b661688555ee298ec186ed3dfe2e2f9e1d5d83
elhajuojy <souirimehdi311@gmail.com>
2023-01-31 19:55:12 +0100

    resgiter logic done

---
+++ api/controllers/Customer/register.php
@@ -0,0 +1,53 @@
+<?php 
+
+use core\App;
+use core\Database;
+use core\Response;
+use core\Validator;
+
+$db = App::resolve(Database::class);
+
+
+$newCustomer = $_POST;
+
+
+$firstname = filterRequest($newCustomer['FirstName']);
+$lastname = filterRequest($newCustomer['LastName']);
+$email = filterRequest($newCustomer['Email']);
+$phone = filterRequest($newCustomer['PhoneNumber']);
+$address = filterRequest($newCustomer['Address']);
+$city = filterRequest($newCustomer['City']);
+$state = filterRequest($newCustomer['State']);
+$randomReference = randomString(10);
+
+if(Validator::string($firstname, 2, 50) && Validator::string($lastname, 2, 50) && Validator::email($email) && Validator::string($phone, 10, 10) && Validator::string($address, 2, 50) && Validator::string($city, 2, 50) && Validator::string($state, 2, 50)){
+   
+    $db->query("insert into Customer (FirstName, LastName, Email, PhoneNumber, Address, City, State, Customer_reference) values (:FirstName, :LastName, :Email, :PhoneNumber, :Address, :City, :State, :Customer_reference)", 
+    [   
+        'FirstName' => $firstname,
+        'LastName' => $lastname,
+        'Email' => $email,
+        'PhoneNumber' => $phone,
+        'Address' => $address,
+        'City' => $city,
+        'State' => $state,
+        'Customer_reference' => $randomReference
+    ]);
+
+    http_response_code(Response::CREATED);
+    echo json_encode(['message' => 'Created', 'Customer_reference' => $randomReference]);
+
+}
+
+else{
+    http_response_code(Response::BAD_REQUEST);
+    echo json_encode(['message' => 'Bad Request']);
+}
+
+//after validating the data, we can insert it into the database
+
+
+
+
+
+// echo json_encode($randomReference);
