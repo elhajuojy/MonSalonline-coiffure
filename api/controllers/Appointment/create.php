26fa4a51f029c8f0db5985735e2b79924d95ad8f
elhajuojy <souirimehdi311@gmail.com>
2023-02-01 11:32:54 +0100

    create appointment done

--- api/controllers/Appointment/create.php
+++ api/controllers/Appointment/create.php
@@ -8,7 +8,12 @@ use core\Response;
 
 //appoiment/create.php information and add them to the database
 
-$newAppoiment = filterRequest($_POST);
+$newAppoiment = $_POST;
 
 $db = App::resolve(Database::class);
 
+// dd($_POST);
+//check if the customer is already in the database
+
+
+$db->query("INSERT INTO Appointment (CustomerID, EmployeeID, AppointmentDate, AppointmentTime, AppointmentStatus, AppointmentType, AppointmentNotes) VALUES (:CustomerID, :EmployeeID, :AppointmentDate, :AppointmentTime, :AppointmentStatus, :AppointmentType, :AppointmentNotes)", $newAppoiment);
