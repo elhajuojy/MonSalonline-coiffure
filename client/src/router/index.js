2f6178b11f6c2f1502c64096e27e8082985ace1d
elhajuojy <souirimehdi311@gmail.com>
2023-02-01 19:46:20 +0100

    add routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -12,10 +12,17 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path:'/login',
+      name : "login",
+      component : ()=>import("../views/LoginView.vue")
+    },
+    {
+      path:'/register',
+      name : "register",
+      component : ()=>import("../views/RegisterView.vue")
     }
   ]
 })
