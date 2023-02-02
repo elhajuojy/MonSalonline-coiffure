c270d11addc637dbe4dd7d927a96614e4e66e0e5
elhajuojy <souirimehdi311@gmail.com>
2023-02-02 17:22:57 +0100

    add swal package

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,8 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 loadFonts()
 
@@ -13,5 +15,6 @@ import './assets/main.css';
 createApp(App)
   .use(router)
   .use(vuetify)
+  .use(VueSweetalert2)
   .use(VueAxios, axios)
   .mount('#app')
