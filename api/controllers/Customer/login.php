efc53025b1900079ff9c723b9eff1dcebe6700a5
elhajuojy <souirimehdi311@gmail.com>
2023-02-03 19:21:19 +0100

    fix headers

--- api/controllers/Customer/login.php
+++ api/controllers/Customer/login.php
@@ -5,8 +5,8 @@ use core\Database;
 use core\Response;
 
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+// header('Access-Control-Allow-Origin: *');
+// header('Content-Type: application/json');
 // header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 $db = App::resolve(Database::class);
 
