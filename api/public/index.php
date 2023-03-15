e2b7f32f53e97342a700578c5ed399657fda6ca3
elhajuojy <souirimehdi311@gmail.com>
2023-03-15 03:46:21 -0700

    assets

--- api/public/index.php
+++ api/public/index.php
@@ -3,34 +3,24 @@
 use core\App;
 use core\Database;
 
-
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Content-Type: application/json');
-
 
 session_start();
 
 const BASE_PATH = __DIR__ . '/../';
 
-
-
 require_once BASE_PATH . 'vendor/autoload.php';
 require_once BASE_PATH . 'functions.php';
 
-
 require base_path('bootstrap.php');
-
-
-
 
 $router = new core\Router();
 $routes = require_once base_path('routes.php');
 
-
-
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-$method =  $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+$method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
